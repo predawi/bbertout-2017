d1caa88d11299c0dcfba23c2dd1cb8ae59a3f8bc
Benjamin Bertout <bbertout@beapi.fr>
2018-01-26 01:21:54 +0100

    add node variant

--- wp-content/themes/bbertout-2017/assets/js/src/scroll.js
+++ wp-content/themes/bbertout-2017/assets/js/src/scroll.js
@@ -3,7 +3,8 @@
  */
 
 // Dependencies
-var Waypoint = require('../vendor/waypoint')
+const inView = require('../vendor/in-view.min.js')
+const Vibrant = require('node-vibrant')
 
 /**
  * [activateMagnetism] Activate magnetic scroll on desktop
@@ -11,28 +12,26 @@ var Waypoint = require('../vendor/waypoint')
 function setWaypoints (mediaQuery) {
   if (mediaQuery.matches) {
     // Trigger img on view
-    var maskedImg = document.getElementsByClassName('highlight__project')
-
-    for (var i = 0; i < maskedImg.length; i++) {
-      Waypoint({
-        element: maskedImg[i],
-        handler: function () {
-          this.element.classList.add('visible')
+    inView('.highlight__project')
+      .on('enter', el => {
+        if (!el.done) {
+          el.classList.add('visible')
 
           // Declare some vars
           var err, palette
-          var dataImg = this.element.getAttribute('data-img')
+          var dataImg = el.getAttribute('data-img')
 
           // Extract colors
           var v = new Vibrant('../assets/img/' + dataImg + '.jpg')
           v.getPalette((err, palette)).then((palette) => {
             // Set vibrant color on elements
-            this.element.querySelector('.highlight__project__panel').style = 'background:' + palette.Vibrant.getHex()
+            el.querySelector('.highlight__project__panel').style = 'background:' + palette.Vibrant.getHex()
           })
-        },
-        offset: 500
+        }
       })
-    }
+      .on('exit', el => {
+        el.done = true
+      })
   }
 }
 
